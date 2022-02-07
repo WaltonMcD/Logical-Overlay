62616bb526bc7cbff12c70fa667bf035d3a6565d
kc <16467859+llKCll@users.noreply.github.com>
2022-02-07 04:29:37 -0600

    Revert "update start.sh"
    This reverts commit 601ad9c942532e36ddde8d69a44d47a9345f5c53.

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ USERNAME=$2    # editable
 NODE_DELAY='sleep 10;'     # editable. How long you have to setup-overlay before nodes attempt to connect to Registry.
 LIB_DIR="$PWD/build/libs/"
 SERVER=""
-PORT='64001'
-REGISTRY_SCRIPT='java -jar Homework-1/build/libs/Homework-1.jar cs455.overlay.Registry server $PORT $MAX_CONN'
+REGISTRY_SCRIPT='java -jar Homework-1/build/libs/Homework-1.jar cs455.overlay.Registry server 64001 $MAX_CONN'
 NODE_SCRIPT=''
 
 if [ -d "$LIB_DIR" ]; then
@@ -25,7 +24,7 @@ if [ -d "$LIB_DIR" ]; then
 		    SERVER="$i"
 		    option='--window --title="'$i'" --command="ssh -t '$USERNAME'@'$SERVER'.cs.colostate.edu '$REGISTRY_SCRIPT' '$SHELL'"'
 		else
-		    NODE_SCRIPT="$NODE_DELAY java -jar Homework-1/build/libs/Homework-1.jar cs455.overlay.Registry node $SERVER.cs.colostate.edu $PORT"
+		    NODE_SCRIPT="$NODE_DELAY java -jar Homework-1/build/libs/Homework-1.jar cs455.overlay.Registry node $SERVER.cs.colostate.edu 64001"
 		    option='--tab --title="'$i'" --command="ssh -t '$USERNAME'@'$i'.cs.colostate.edu '$NODE_SCRIPT' '$SHELL'"'
 		fi
 
